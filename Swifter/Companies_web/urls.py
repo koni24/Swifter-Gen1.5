eecac7e1704a2974f7fcdec678c63f42dd1f81ed
Ketan Mishra <78914549+inventingphoenix@users.noreply.github.com>
2022-03-25 17:49:25 +0530

    Added views, urls and webpages to the project

--- Swifter/Companies_web/urls.py
+++ Swifter/Companies_web/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-
+from Companies_web import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
+  path('signup',views.Signup, name="signup"),
+  path('login',views.login, name="login"),
+  path('logout',views.logout, name="logout"),
 ]
