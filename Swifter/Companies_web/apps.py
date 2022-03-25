3ec6891edc640e6b576803453158b45620bdb33b
Ketan Mishra <78914549+inventingphoenix@users.noreply.github.com>
2022-03-25 16:31:37 +0530

    first commit swifter 1.5v

---
+++ Swifter/Companies_web/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CompaniesWebConfig(AppConfig):
+    name = 'Companies_web'
---
+++ Swifter/Transporters_web/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TransportersWebConfig(AppConfig):
+    name = 'Transporters_web'
